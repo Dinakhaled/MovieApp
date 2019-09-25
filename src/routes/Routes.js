0a2609501271d0a2c318c6ab3a243ef164ec74b5
Dinakhaled <dina.elghndour@gmail.com>
2019-09-25 20:25:14 +0200

    add network store folders

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,16 +1,14 @@
-  
-import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
-import history from './History';
-import App from '../containers/App';
-import HomePage from '../containers/homePage/HomePage';
+import React from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import history from "./History";
+// ========= Components =========
+import HomePage from "../containers/homePage/HomePage";
 
 const Routes = (
   <Router history={history}>
     <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/home" component={HomePage} />
-      <Redirect from='*' to='/' />
+      <Route path="/" exact component={HomePage} />
+      <Redirect from="*" to="/" />
     </Switch>
   </Router>
 );
