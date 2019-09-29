ce510d781b71cc5b6b6c26b96483ee23fc1b83ee
Dina Elghndour <Dina.Elghndour@ibtikar.com>
2019-09-29 16:59:54 +0200

    Add defualt font sizes

---
+++ src/components/card/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Card.scss';
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <h1>Card</h1>
+    );
+  }
+}
+ 
+export default Card;
