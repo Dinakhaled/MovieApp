3b041f9ea1e61dd76dbfc6812df5fa42f8b15789
Dinakhaled <dina.elghndour@gmail.com>
2019-10-08 21:58:38 +0200

    button style edit

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,24 @@ import React from "react";
 import { Router } from "react-router-dom";
 import history from "../routes/History";
 import Routes from "../routes/Routes";
+import List from "../components/list/List";
 // ========== General styles ==========
 import "bootstrap/scss/bootstrap.scss";
 import "./App.scss";
 
+const list = [
+  { content: "one", icon: "", id: 1 },
+  { content: "two", icon: "", id: 2 },
+  { content: "three", icon: "", id: 3 }
+];
+
 const App = () => {
-  return <Router history={history}>{Routes}</Router>;
+  return (
+    <Router history={history}>
+      <List list={list} />
+      {Routes}
+    </Router>
+  );
 };
 
 export default App;
