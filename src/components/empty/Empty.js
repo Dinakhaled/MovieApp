e2576e018bb31a4de6b3fb2b14582f18bc477b25
Dina Elghndour <Dina.Elghndour@ibtikar.com>
2019-11-06 17:25:38 +0200

    single image height adjustment

--- src/components/empty/Empty.js
+++ src/components/empty/Empty.js
@@ -6,7 +6,11 @@ const Empty = props => {
     <div className="text-center">
       <h1 className="h1-2-light">Sorry!</h1>
       <h2 className="h2-2">{title}</h2>
-      <img src="/assets/images/not-found.png" className="w-50" />
+      <img
+        src="/assets/images/not-found.png"
+        className="w-50"
+        alt="not-found"
+      />
     </div>
   );
 };
