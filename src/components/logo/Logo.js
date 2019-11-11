afc42d1e06aec77073dc15b83bb2a4721e00260f
Dina Elghndour <Dina.Elghndour@ibtikar.com>
2019-11-11 17:19:50 +0200

    Handle responsivness for card grid

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Logo.scss";
+import history from "../../routes/History";
+
+const handleClick = () => {
+  history.push("/");
+};
 
 const Logo = () => {
-  return <div className="logo"></div>;
+  return <div className="logo" onClick={handleClick}></div>;
 };
 
 export default Logo;
